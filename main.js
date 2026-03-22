98b7c72c12f168d499f30d81a5a5a662bbd0004f
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-03-22 02:00:19 -0300

    Add: Schema to User models

--- main.js
+++ main.js
@@ -5,13 +5,16 @@ const app = express()
 
 const port = 7000
 
+
+const User = require('./src/models/user.model')
+
 //http://localhost:7000/
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello world!')
+// })
 
-mongoose.connect('mongodb+srv://josefina:josefina@clustercero.ea9ggib.mongodb.net/')
+mongoose.connect('mongodb+srv://josefina:josefina@clustercero.ea9ggib.mongodb.net/elFacon_Db')
     .then(() => {
         console.log('Conectada a la base de datos')
         app.listen(port, () => {
