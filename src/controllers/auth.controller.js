79a1f1874f453cb3e2f7032552cbe4eab4bc4245
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-03-23 11:01:28 -0300

    Add: controller register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,2 +1,31 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/user.model')
+
+const register = async (req, res) => {
+    //campos de donde vienen los datos
+    const {
+        username,
+        password,
+        email,
+        firstName,
+        lastName
+    } = req.body
+
+    //encriptar password
+    const salt = bcrypt.genSaltSync(8);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    console.log( salt, password, hashedPassword)
+
+    //creacion del usuario
+    const user = new User({
+        username, 
+        password: hashedPassword,
+        email,
+        firstName, 
+        lastName
+    })
+
+    await user.save()
+
+    //generar estado
+}
