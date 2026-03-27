cdf06b4701cad5735626dba47d50699a4496319d
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-03-27 00:23:24 -0300

    Add: verify JWT validation

---
+++ src/middlewares/auth.validations.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = require('../common/constants')
+
+const verifyJWT = (req, res, next) => {
+
+    if(!req.header.authorization)
+        return res.json({message: 'Unauthorized'})
+
+    const authorization = req.headers.authorization
+    const token = authorization.split(" ")[1]
+
+    jwt.verify(token, JWT_SECRET,(err, decoded) => {
+       if(err) {
+        return res.json({message: 'Token invalido'});
+       } 
+       //si el token es valido, se guardan los datos del usuario en req
+       next();
+    })
+
+}
+
+module.exports = {
+    verifyJWT
+}
