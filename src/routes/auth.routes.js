ff371f047039254ca36144e9fd8e668ba4686c62
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-03-26 21:51:54 -0300

    Add: endpoints and secret key

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,19 @@
 const { Router } = require('express')
+const { registerUser, loginUser } = require('../controllers/auth.controller')
 
 //http://localhost:7000/auth
 const authRouter = Router()
+
+authRouter.post('/register', 
+    [], 
+    registerUser
+)
+
+authRouter.post('/login', 
+    [],
+    loginUser
+ )
+
 
 
 
