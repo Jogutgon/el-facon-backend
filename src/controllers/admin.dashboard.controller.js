623690c41a3b508d434470d398930d0bbbadaf80
Josefina Gutierrez Gonzalez <jogugonzalez@gmail.com>
2026-04-28 12:32:18 -0300

    Add: new file admin dashboard and path

---
+++ src/controllers/admin.dashboard.controller.js
@@ -0,0 +1,24 @@
+const User = require('../models/user.model')
+const Reservation = require('../models/reservation.model')
+
+const adminDashboard = async (req, res) => {
+    try {
+        const totalUsers = await User.countDocuments( { isAdmin: false});
+        const totalReservations = await Reservation.countDocuments();
+        const inactiveUsers = await User.countDocuments({ status: false });
+
+        res.json({
+            totalUsers,
+            totalReservations,
+            inactiveUsers
+            }
+        )
+
+    } catch (error) {
+        res.status(500).json({
+            message: "ERROR AL BUSCAR LAS ESTADISTICAS"
+        })
+    }
+}
+
+module.exports = adminDashboard
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -2,6 +2,8 @@ const { Router } = require('express')
 const { verifyJWT } = require('../middlewares/auth.validations')
 const { findAllUsers, updateUser, statusUser, deleteUserById } = require('../controllers/admin.controller')
 const { AllReservations, updateReservation, deleteReservationByAdmin } = require('../controllers/reservation.controller')
+const isAdmin = require('../middlewares/isAdmin.validation')
+const adminDashboard = require('../controllers/admin.dashboard.controller')
 
 
 //http://localhost:7000/admin
@@ -42,7 +44,14 @@ adminRouter.put('/update-reservation/:id',
 
 adminRouter.delete('/delete-reservation/:id', 
     verifyJWT, 
-    deleteReservationByAdmin
+    deleteReservationByAdmin,
+    isAdmin
+)
+
+adminRouter.get('/dashboard', 
+    verifyJWT,
+    isAdmin,
+    adminDashboard
 )
 
 
